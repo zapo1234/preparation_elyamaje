0792349b45da39ea69687b822a4e6cd2644a5919
martial <martial@elyamaje.com>
2023-05-19 12:08:50 +0200

    p

--- resources/views/refreshtiers.blade.php
+++ resources/views/refreshtiers.blade.php
@@ -8,7 +8,7 @@
 			<div class="page-wrapper">
 				<div class="page-content">
 					<div class="page-breadcrumb d-sm-flex align-items-center mb-2">
-						<div class="breadcrumb-title pe-3">Commandes en préparation</div>
+						<div class="breadcrumb-title pe-3">Mise à jours clients dolibarr</div>
 						<div class="pe-3 number_order_pending"></div>
 
 					</div>
