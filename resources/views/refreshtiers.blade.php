eb6b968b7d0614b9377231e90d757d732b148ce0
martial <martial@elyamaje.com>
2023-05-19 12:14:59 +0200

    p

--- resources/views/refreshtiers.blade.php
+++ resources/views/refreshtiers.blade.php
@@ -16,7 +16,19 @@
 						<div class="card card_table_mobile_responsive">
 							<div class="card-body">
 								<div class="table-responsive">
-
+                                <table id="example" class="w-100 table_mobile_responsive table table-striped table-bordered">
+										<thead>
+											<tr>
+												<th scope="col">Mettre à jours les clients</th>
+												<th scope="col">Action</th>
+											
+											</tr>
+                                            <tr>
+                                                <td>Tiers dolibarr</td>
+                                             <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Import tiers</button> </td>
+                                            </tr>
+										</thead>
+									</table>
 									
 								</div>
 							</div>
