3008d6f138b244de2b0580205a4bae7d5cd3bacc
martial <martial@elyamaje.com>
2023-06-08 14:50:54 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -265,6 +265,9 @@ class TransferOrder
                      $data_list_product[$values['id']] = $values['barcode'];
                       // tableau associatve entre ref et label product....
                   }
+
+
+                  dd($data_list_product);
                  
                     // recupére les orders des données provenant de  woocomerce
                     // appel du service via api
