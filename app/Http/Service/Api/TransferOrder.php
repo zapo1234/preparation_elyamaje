8e6f366da66b697aa6f492b96744e7c24f4ed01e
martial <martial@elyamaje.com>
2023-06-09 11:58:59 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -334,10 +334,10 @@ class TransferOrder
                                       if($fk_product!=""){
                                          // details  array article libéllé(product sur la commande) pour dolibarr.
                                           $data_product[] = [
-                                            "multicurrency_subprice"=> floatval($values['subtotal']),
-                                            "multicurrency_total_ht" => floatval($values['subtotal']),
-                                            "multicurrency_total_tva" => floatval($values['total_tax']),
-                                            "multicurrency_total_ttc" => floatval($values['total']+$values['total_tax']),
+                                           // "multicurrency_subprice"=> floatval($values['subtotal']),
+                                           // "multicurrency_total_ht" => floatval($values['subtotal']),
+                                          //  "multicurrency_total_tva" => floatval($values['total_tax']),
+                                         //   "multicurrency_total_ttc" => floatval($values['total']+$values['total_tax']),
                                             "product_ref" => $ref, // reference du produit.(sku wwocommerce/ref produit dans facture invoice)
                                             "product_label" =>$values['name'],
                                             "qty" => $values['quantity'],
