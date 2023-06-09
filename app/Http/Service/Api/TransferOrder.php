64ec1afa8c6926be7e536cd6489596ed645c997b
martial <martial@elyamaje.com>
2023-06-09 14:36:29 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -621,14 +621,15 @@ class TransferOrder
         ];
            
              $fac = intval($inv+1);
-             dd($fac);
-             $array_ids_facture = array($inv,$inv+1);
-             dump($array_ids_facture[0]);
-             dd($array_ids_facture[1]);
+              $array_ids_facture = array($inv,$fac);
+
+              dd($array_ids_facture);
+             
                // valider les facture dans dolibar
               $this->api->CallAPI("POST", $apiKey, $apiUrl."invoices/".$inv."/validate", json_encode($newCommandeValider));
               // mettre le statut en payé dans la facture  dolibar
               $this->api->CallAPI("PUT", $apiKey, $apiUrl."invoices/".$inv, json_encode($newCommandepaye));
+              
                // Lier les factures dolibar  à un moyen de paiement et bank.
                $this->api->CallAPI("POST", $apiKey, $apiUrl."invoices/".$inv."/payments", json_encode($newbank));
     }
