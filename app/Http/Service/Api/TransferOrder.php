5fcc5538e791a2e33bc03e3f22ae913cfec4fc65
martial <martial@elyamaje.com>
2023-06-12 14:31:33 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -196,7 +196,7 @@ class TransferOrder
      */
       public function Transferorder($orders)
       {
-             
+              dd($orders);
              // excercer un get et post et put en fonction des status ...
                // recuperer les données api dolibar copie projet tranfer x.
                $method = "GET";
