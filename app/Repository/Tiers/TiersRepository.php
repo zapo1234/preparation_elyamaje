db36d00b72429fb1e4cc94253400eefac6230fa9
martial <martial@elyamaje.com>
2023-06-13 17:07:51 +0200

    p

--- app/Repository/Tiers/TiersRepository.php
+++ app/Repository/Tiers/TiersRepository.php
@@ -51,10 +51,12 @@ class TiersRepository implements TiersInterface
     public function gettiersid($id)
     {
       // recupérer 
-       //$data =  DB::table('tiers')->select('nom')->where('socid','=',$id)->get();
+       $data =  DB::table('tiers')->select('nom')->where('socid','=',$id)->get();
        // transformer les retour objets en tableau
-       $data = Tier::find($id);
-       return $data;
+       $list = json_encode($data);
+       $lists = json_decode($data,true);
+       
+       return $lists;
 
 
     }
