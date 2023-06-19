4695e0e003bb3969291dfb0497a40ee21b1fb044
martial <martial@elyamaje.com>
2023-06-19 15:48:54 +0200

    ajout model distributeur

--- app/Http/Controllers/TiersController.php
+++ app/Http/Controllers/TiersController.php
@@ -41,7 +41,7 @@ class TiersController extends BaseController
     public function postiers()
     { 
         // recupérer le traitement des tiers pour les inserts dans la table...
-        dd($orderdol);
+        dd($this->orderdol->getordersdol());
         $this->tiers->insertiers();
         dd('zapo');
         $this->orders->Transferorders();
--- app/Http/Service/Api/TransfertOrderdol.php
+++ app/Http/Service/Api/TransfertOrderdol.php
@@ -47,12 +47,52 @@ class TransfertOrderdol
 
                // recupérer les orders depuis la prod
                dd($lists);
-               
-               
-              
-               
-   }
+               //construire les données à flush dans les tables orders_dol et order_product_dol.
+               $data_ids =[];// recupérer les ids socid et recupérer l'utilisateur ensuite depuis la table tiers.
+               $data_orders_dol =[];
+               $data_orders_product =[];
 
+               foreach($lists as $value){
+                  
+                $x = date('Y-m-d', $vals['date_creation']);
+                 // ajouter un jour $x
+                $x1  = date("Y-m-d", strtotime($x.'+ 1 days'));// date de création de la commande via dolibarr.
+
+                $data_ids[] = $value['socid'];
+                $data_order_dol[] =[
+                 'socid' => $value['socid'],
+                 'date_creation'=> $x1,
+                 'order_id'=>$value['id'],
+                 'total_ht' =>floatval($value['total_ht']),
+                 'total_tva'=>floatval($value['total_tva']),
+                 'total_ttc'=>floatval($value['total_ttc'])
+                
+                 ];
+
+                  foreach($value['lines'] as $val) {
+                   
+                    $data_orders_dol[] =[
+                     'order_id' => $value['id'],
+                     'data_product'=>[
+                      "ref"=>$val['ref'],
+                      "product_label"=>$val['product_label'],
+                      "product_barcode"=>$val['product_barcode'],
+
+
+                     ]
+
+
+                     ];
+
+                  
+
+                    }
+               
+               
+         }    
+               
+
+        }
 
 }
 
