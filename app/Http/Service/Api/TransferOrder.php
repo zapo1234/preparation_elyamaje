c65064af32792f3f7ead933a6cf9f12401563719
martial <martial@elyamaje.com>
2023-06-27 17:35:21 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -120,6 +120,7 @@ class TransferOrder
                  // recuperer les ids commandes
                  $ids_commande = $this->commande->getAll(); // tableau pour recupérer les id_commande 
                  $key_commande = $this->commande->getIds();// lindex les ids commande existant.
+                 dd($key_commande);
                  // recupérer le tableau de ids
                  $ids_commandes =[];
               
