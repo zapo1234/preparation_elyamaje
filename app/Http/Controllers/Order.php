75721eda401a03c6bee39109205bf00bf59f87f4
adrien <adrien@elyamaje.com>
2023-06-28 16:56:53 +0200

    comment doublon function call in validWrapOrder

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -381,7 +381,7 @@ class Order extends BaseController
             $orders = $this->woocommerce->transformArrayOrder($order);
           
             // envoi des données pour créer des facture via api dolibar....
-            $this->factorder->Transferorder($orders);
+           
             // if($request->post('from_label') != "true"){
                  $this->factorder->Transferorder($orders);
                 // Modifie le status de la commande sur Woocommerce en "Prêt à expédier"
