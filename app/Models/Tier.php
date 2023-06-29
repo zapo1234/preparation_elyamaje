4063a325b1c57cc125be6a4374f2bfb86ca8017f
martial <martial@elyamaje.com>
2023-06-29 15:54:44 +0200

    p

--- app/Models/Tier.php
+++ app/Models/Tier.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-
-class Tier extends Model
+class Tier extends Authenticatable
 {
     use HasFactory, Notifiable;
 
