d80e98161a1cf050a0997f75b89ecfe3d704f572
martial <martial@elyamaje.com>
2023-06-29 15:56:15 +0200

    p

--- app/Repository/Tiers/TiersRepository.php
+++ app/Repository/Tiers/TiersRepository.php
@@ -67,6 +67,8 @@ class TiersRepository implements TiersInterface
         // transformer les retour objets en tableau
         $list = json_encode($data);
         $lists = json_decode($data,true);
+
+        dd($lists);
         $list_code =[];
         
         foreach($lists as $key =>  $values){
@@ -104,42 +106,6 @@ class TiersRepository implements TiersInterface
 
 
            $array_tiers = $this-> getallsocid();
-
-    
-
-            foreach($lists as $key=>$values){
-               
-               if($this->testing($array_tiers,$values['id'])==false){
-               
-                 if(!in_array($values['id'],$data_ids)) {
-                    
-                    if($values['client']==1 OR $values['client']==3){
-                          $x = date('Y-m-d H:i:s', $values['date_creation']);
-                          $x1 = date("Y-m-d", strtotime($x.'+ 0 days'));
-                          $x2 = date("Y-m-d H:i:s", strtotime($x.'+ 0 days')); // AJOUTER +1 ans MAI 2024
-                          $y = date('Y-m-d H:i:s', $values['date_modification']);
-                          $y2 = date("Y-m-d H:i:s", strtotime($y.'+ 0 days')); // AJOUTER +1 en Mai 2024 rappel
-                           // Insert dasn la table.
-                           $tier = new Tier;
-                           $tier->nom = $values['name'];
-                           $tier->prenom = $values['name_alias'];
-                           $tier->socid = $values['id'];
-                           $tier->email = $values['email'];
-                           $tier->code_client = $values['code_client'];
-                           $tier->phone = $values['phone'];
-                           $tier->adresse = $values['address'];
-                           $tier->zip_code = $values['zip'];
-                           $tier->ville = $values['town'];
-                            // save clients
-                           $tier->save();
-                 
-                    }
-                
-                }
-                
-               }
-             
-           }
      
 
       }
