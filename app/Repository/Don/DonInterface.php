fc0571feefc0ba7ea764ff72f5e384a439b2aac7
martial <martial@elyamaje.com>
2023-06-30 17:10:18 +0200

    p

--- app/Repository/Don/DonInterface.php
+++ app/Repository/Don/DonInterface.php
@@ -3,7 +3,7 @@
 namespace App\Repository\Don;
 
 
-interface DonsproductInterface
+interface DonInterface
 {
      public function inserts($first_name,$last_name,$email,$order_id,$coupons,$total_order,$date_order);
 
