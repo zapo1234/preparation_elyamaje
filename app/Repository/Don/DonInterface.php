6057ed2e03147188dc3a2480b4a56d4e14b60652
martial <martial@elyamaje.com>
2023-06-30 14:08:04 +0200

    p

--- app/Repository/Don/DonInterface.php
+++ app/Repository/Don/DonInterface.php
@@ -7,7 +7,7 @@ interface DonInterface
 {
      public function inserts($first_name,$last_name,$email,$order_id,$coupons,$total_order,$date_order);
 
-     public function getiers();
+     public function gettiers();
      
 }
 
