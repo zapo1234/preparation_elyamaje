9c3b6c39a5e66de154c0a03bf6b02d927d860c89
martial <martial@elyamaje.com>
2023-07-03 12:11:58 +0200

    p

--- app/Models/Donsproduct.php
+++ app/Models/Donsproduct.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class DonsProduct extends Authenticatable
+
+class DonsProduct extends Model
 {
     use  HasFactory, Notifiable;
 
