682841f09dc5c74a60e82d47908c238ef6e09b30
martial <martial@elyamaje.com>
2023-07-03 11:44:34 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -422,6 +422,7 @@ class TransferOrder
                        // TRAITER LES données des cadeaux 
                        // merger le client et les data coupons
                         dd($data_kdo);
+                        dump($data_lines);
                       
                       
               
