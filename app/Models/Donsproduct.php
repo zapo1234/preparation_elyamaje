fb652dcd9b3e4dd43802a961ece83cb7c14c222c
martial <martial@elyamaje.com>
2023-07-03 12:15:03 +0200

    p

--- app/Models/Donsproduct.php
+++ app/Models/Donsproduct.php
@@ -5,24 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class DonsProduct extends Model
 {
-    use  HasFactory, Notifiable;
-
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'order_id',
-        'id_product',
-        'label',
-        'quantite',
-        'real_price',
-        
-     ];
-
+      'id',
+      'order_id',
+      'product_id',
+      'label',
+      'quantitty',
+      'real_price',
+    
+    ];
 }
-   
