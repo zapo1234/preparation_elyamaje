70cce9a30ae3f8fc12ba9b8be02ac86941b1ae69
adrien <adrien@elyamaje.com>
2023-07-07 11:10:33 +0200

    email design

--- resources/views/email/resetpassword.blade.php
+++ resources/views/email/resetpassword.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<table style="color:#000000" width="100%" border="0" cellspacing="0" cellpadding="0">
+<table style="font-family:math;color:#000000" width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
         <td align="center">
             <table style="max-width:600px;border-style:solid; border-width:1px; border-color:#c9c9c9;" width="100%" border="0" cellspacing="0" cellpadding="25">
