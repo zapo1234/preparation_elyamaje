c6abb87f5826c8c35640462ae890126e28f2dd2e
adrien <adrien@elyamaje.com>
2023-07-07 11:05:30 +0200

    email design

--- resources/views/email/resetpassword.blade.php
+++ resources/views/email/resetpassword.blade.php
@@ -5,11 +5,11 @@
 <table style="color:#000000" width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
         <td align="center">
-            <table style="max-width:600px;border-style:solid; border-width:1px; border-color:#c9c9c9;" width="100%" border="0" cellspacing="0" cellpadding="0">
+            <table style="max-width:600px;border-style:solid; border-width:1px; border-color:#c9c9c9;" width="100%" border="0" cellspacing="0" cellpadding="25">
                 <tr>
                     <td align="center">
                         <a href="">
-                            <img src="{{ asset('assets/images/elyamaje_logo_long_noir.png') }}" width="150px"; height="auto"; style="margin-top:35px;";>
+                            <img src="{{ asset('assets/images/elyamaje_logo_long_noir.png') }}" width="150px"; height="auto"; style="margin-top:10px;";>
                         </a>     
 
                         <img src="{{ asset('assets/images/bg-themes/bg-email.jpg') }}" width="100%"; height="auto" style="border-top: 1px solid #c9c9c9; margin-top:25px;";>     
