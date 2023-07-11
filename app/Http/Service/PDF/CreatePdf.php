d0ba23bd58c5f181487149d3e323ddb259c9fa09
adrien <adrien@elyamaje.com>
2023-07-11 10:29:50 +0200

    update request history and add information picked products

--- app/Http/Service/PDF/CreatePdf.php
+++ app/Http/Service/PDF/CreatePdf.php
@@ -24,16 +24,23 @@ class CreatePdf
         $table .='<tr>';
             $table.='<th style="border:1px solid black" bgcolor="white">Nom</th>';
             $table.='<th style="border:1px solid black" bgcolor="white">Poste</th>';
-            $table.='<th style="border:1px solid black" bgcolor="white">Commandes Préparées</th>';
-            $table.='<th style="border:1px solid black"bgcolor="white">Commandes Emballées</th>';
+            $table.='<th style="border:1px solid black" bgcolor="white">Préparées</th>';
+            $table.='<th style="border:1px solid black" bgcolor="white">Total</th>';
+            $table.='<th style="border:1px solid black"bgcolor="white">Produits</th>';
+            $table.='<th style="border:1px solid black"bgcolor="white">Emballées</th>';
+            $table.='<th style="border:1px solid black" bgcolor="white">Total</th>';
         $table .='</tr>';
 
         foreach ($data as $row) {
             $table .= '<tr>';
                 $table .= '<td  style="border:1px solid black" bgcolor="white">' . $row['name'] . '</td>';
-                $table .= '<td  style="border:1px solid black" bgcolor="white">' . $row['user_poste'] ?? '' . '</td>';
-                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['prepared_order'] . '</td>';
-                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['finished_order'] . '</td>';
+                $table .= '<td  style="border:1px solid black" bgcolor="white">' . implode(',',$row['poste']) ?? '' . '</td>';
+                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . implode(',',$row['prepared_order']) . '</td>';
+                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['prepared_count'] . '</td>';
+                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['items_picked'] . '</td>';
+                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . implode(',',$row['finished_order']) . '</td>';
+                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['finished_count']. '</td>';
+
             $table .= '</tr>';
         }
         $table .= '</table>';
@@ -42,36 +49,7 @@ class CreatePdf
         $this->dompdf->loadHtml($table);
         $this->dompdf->render();
         return $this->dompdf->stream($name.'.pdf');
-    }
-
-    public function generateHistoryOrdersCloseDay($data, $date_historique){
-
-        $date_historique = date("d-m-Y", strtotime($date_historique));
-        $table = '<h3 style="text-align:center">'.$date_historique.'</h3></br>';
-        $table .= '<table style="width: 100%;" cellpadding="1" cellspacing="1">';
-        $table .='<tr>';
-            $table.='<th style="border:1px solid black" bgcolor="white">Nom</th>';
-            $table.='<th style="border:1px solid black" bgcolor="white">Poste</th>';
-            $table.='<th style="border:1px solid black" bgcolor="white">Commandes Préparées</th>';
-            $table.='<th style="border:1px solid black"bgcolor="white">Commandes Emballées</th>';
-        $table .='</tr>';
-
-        foreach ($data as $row) {
-            $table .= '<tr>';
-                $table .= '<td  style="border:1px solid black" bgcolor="white">' . $row['name'] . '</td>';
-                $table .= '<td  style="border:1px solid black" bgcolor="white">' . $row['user_poste'] ?? '' . '</td>';
-                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['prepared_count'] . '</td>';
-                $table .= '<td  style="word-break:break-word;border:1px solid black" bgcolor="white">' . $row['finished_count'] . '</td>';
-            $table .= '</tr>';
-        }
-        $table .= '</table>';
-  
-        $name = 'preparation_'.$date_historique;
-        $this->dompdf->loadHtml($table);
-        $this->dompdf->render();
-        return $this->dompdf->stream($name.'.pdf');
-    }
-      
+    } 
   }
 
 
