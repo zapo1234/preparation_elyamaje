488be86126c0be0df4aeac3214c39b551f5b5bca
adrien <adrien@elyamaje.com>
2023-07-12 17:27:53 +0200

    logo svg email

--- resources/views/email/newAccount.blade.php
+++ resources/views/email/newAccount.blade.php
@@ -10,7 +10,7 @@
                     <td align="center">
                        
                         <a href="">
-                            <img src="{{ asset('assets/images/elyamaje_logo_long_noir.svg') }}" width="150px"; height="auto"; style="margin-top:10px;";>
+                            <img src="{{ asset('assets/images/elyamaje_logo_long_noir.png') }}" width="150px"; height="auto"; style="margin-top:10px;";>
                         </a>     
 
                         <img src="{{ asset('assets/images/bg-themes/bg-email.jpg') }}" width="100%"; height="auto" style="border-top: 1px solid #c9c9c9; margin-top:25px;";>     
