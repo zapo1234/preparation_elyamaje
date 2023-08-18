a0d2be448937063a38be0db367a387b850a514de
adrien <adrien@elyamaje.com>
2023-08-18 14:26:19 +0200

    printer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Controllers\Order;
 use App\Repository\Role\RoleRepository;
 use App\Repository\User\UserRepository;
@@ -16,7 +17,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    
+
     private $orderController;
     private $orders;
     private $users;
@@ -25,69 +26,74 @@ class Controller extends BaseController
     private $products;
     private $printer;
 
-    public function __construct(Order $orderController, 
+    public function __construct(
+        Order $orderController,
         UserRepository $users,
         RoleRepository $role,
         OrderRepository $orders,
         CategoriesRepository $categories,
         ProductRepository $products,
         PrinterRepository $printer
-    ){
-      $this->orderController = $orderController;
-      $this->users = $users;
-      $this->role = $role;
-      $this->orders = $orders;
-      $this->categories = $categories;
-      $this->products = $products;
-      $this->printer = $printer;
+    ) {
+        $this->orderController = $orderController;
+        $this->users = $users;
+        $this->role = $role;
+        $this->orders = $orders;
+        $this->categories = $categories;
+        $this->products = $products;
+        $this->printer = $printer;
     }
-    
-     // INDEX ADMIN
-    public function index(){
+
+    // INDEX ADMIN
+    public function index()
+    {
         $teams = $this->users->getUsersByRole([2, 3, 5]);
         $teams_have_order = $this->orders->getUsersWithOrder()->toArray();
         $products =  $this->products->getAllProductsPublished();
         $number_preparateur = 0;
-        
-        foreach($teams as $team){
-            foreach($team['role_id'] as $role){
-                if($role == 2){
+
+        foreach ($teams as $team) {
+            foreach ($team['role_id'] as $role) {
+                if ($role == 2) {
                     $number_preparateur = $number_preparateur + 1;
                 }
             }
-        }   
+        }
 
         $roles = $this->role->getRoles();
         return view('index', ['teams' => $teams, 'products' => $products, 'roles' => $roles, 'teams_have_order' => $teams_have_order, 'number_preparateur' => $number_preparateur]);
     }
 
     // INDEX CHEF D'ÉQUIPE
-    public function dashboard(){
+    public function dashboard()
+    {
         $teams = $this->users->getUsersByRole([2, 3, 5]);
         $teams_have_order = $this->orders->getUsersWithOrder()->toArray();
         $products =  $this->products->getAllProductsPublished();
         $number_preparateur = 0;
-         
-         foreach($teams as $team){
-             foreach($team['role_id'] as $role){
-                 if($role == 2){
-                     $number_preparateur = $number_preparateur + 1;
-                 }
-             }
-         }   
- 
+
+        foreach ($teams as $team) {
+            foreach ($team['role_id'] as $role) {
+                if ($role == 2) {
+                    $number_preparateur = $number_preparateur + 1;
+                }
+            }
+        }
+
         $roles = $this->role->getRoles();
         return view('leader.dashboard', ['teams' => $teams, 'products' => $products, 'roles' => $roles, 'teams_have_order' => $teams_have_order, 'number_preparateur' => $number_preparateur]);
     }
 
     // CONFIGURATION ADMIN
-    public function categories(){
+    public function categories()
+    {
         $categories = $this->categories->getAllCategories();
         return view('admin.categories', ['categories' => $categories]);
     }
 
     // CONFIGURATION LIST PRODUCTS ADMIN
-    public function products(){
+    public function products()
+    {
         // Get all products
         $products = $this->products->getAllProducts();
         // Get all categories products
@@ -97,27 +103,29 @@ class Controller extends BaseController
     }
 
     // PRÉPARATEUR COMMANDES CLASSIQUES
-    public function orderPreparateur(){
+    public function orderPreparateur()
+    {
         $printer = $this->printer->getPrinterByUser(Auth()->user()->id);
         $orders = $this->orderController->getOrder();
-        $order_process = [] ;
+        $order_process = [];
         $orders_waiting_to_validate = [];
         $orders_validate = [];
 
-        foreach($orders as $order){
-            if($order['details']['status'] == "waiting_to_validate"){
+        foreach ($orders as $order) {
+            if ($order['details']['status'] == "waiting_to_validate") {
                 $orders_waiting_to_validate[] = $order;
-            } else if($order['details']['status'] == "waiting_validate"){
+            } else if ($order['details']['status'] == "waiting_validate") {
                 $orders_validate[] = $order;
             } else {
                 $order_process[] = $order;
             }
         }
 
-        return view('preparateur.index_preparateur', ['user' => Auth()->user()->name, 
-            'orders_waiting_to_validate' => $orders_waiting_to_validate, 
-            'orders_validate' => $orders_validate, 
-            'orders' => isset($order_process[0]) ? $order_process[0] : [] /* Show only first order */, 
+        return view('preparateur.index_preparateur', [
+            'user' => Auth()->user()->name,
+            'orders_waiting_to_validate' => $orders_waiting_to_validate,
+            'orders_validate' => $orders_validate,
+            'orders' => isset($order_process[0]) ? $order_process[0] : [] /* Show only first order */,
             'number_orders' =>  count($order_process),
             'number_orders_waiting_to_validate' =>  count($orders_waiting_to_validate),
             'number_orders_validate' =>  count($orders_validate),
@@ -126,27 +134,29 @@ class Controller extends BaseController
     }
 
     // PRÉPARATEUR COMMANDES DISTRIBUTEURS
-    public function ordersDistributeurs(){
+    public function ordersDistributeurs()
+    {
         $printer = $this->printer->getPrinterByUser(Auth()->user()->id);
         $orders = $this->orderController->getOrderDistributeur();
-        $order_process = [] ;
+        $order_process = [];
         $orders_waiting_to_validate = [];
         $orders_validate = [];
 
-        foreach($orders as $order){
-            if($order['details']['status'] == "waiting_to_validate"){
+        foreach ($orders as $order) {
+            if ($order['details']['status'] == "waiting_to_validate") {
                 $orders_waiting_to_validate[] = $order;
-            } else if($order['details']['status'] == "waiting_validate"){
+            } else if ($order['details']['status'] == "waiting_validate") {
                 $orders_validate[] = $order;
             } else {
                 $order_process[] = $order;
             }
         }
 
-        return view('preparateur.distributeur.index_preparateur', ['user' => Auth()->user()->name, 
-            'orders_waiting_to_validate' => $orders_waiting_to_validate, 
-            'orders_validate' => $orders_validate, 
-            'orders' => isset($order_process[0]) ? $order_process[0] : [] /* Show only first order */, 
+        return view('preparateur.distributeur.index_preparateur', [
+            'user' => Auth()->user()->name,
+            'orders_waiting_to_validate' => $orders_waiting_to_validate,
+            'orders_validate' => $orders_validate,
+            'orders' => isset($order_process[0]) ? $order_process[0] : [] /* Show only first order */,
             'number_orders' =>  count($order_process),
             'number_orders_waiting_to_validate' =>  count($orders_waiting_to_validate),
             'number_orders_validate' =>  count($orders_validate),
@@ -155,7 +165,8 @@ class Controller extends BaseController
     }
 
     // INDEX EMBALLEUR 
-    public function wrapOrder(){
+    public function wrapOrder()
+    {
         return view('emballeur.index');
     }
 }
--- assets/js/history_preparateur.js
+++ assets/js/history_preparateur.js
@@ -1,23 +1,23 @@
-$('body').on('click', '.show_order_history', function() {
+$('body').on('click', '.show_order_history', function () {
     var id = $(this).attr('data-order')
 
-    $(".order_"+id).modal('show')
+    $(".order_" + id).modal('show')
 })
 
 
-$('body').on('click', '.show_order_history_code', function() {
+$('body').on('click', '.show_order_history_code', function () {
     var id = $(this).attr('data-order')
 
     var product = $(this).attr('data-product')
     var customer_name = $(this).attr('data-customer')
 
-    const href = id+","+product+","+customer_name;
+    const href = id + "," + product + "," + customer_name;
     const size = 150;
-    
+
     // $(".info_order").text("#Commande "+id+" - "+pick_items.length+" Produit(s)"+" - "+customer_name)
     var list_products = ""
-    $(".order_"+id+" .product_order" ).each(function() {
-        list_products += '<span>'+$( this ).children( "div" ).children( "span" ).text()+' - x'+$( this ).children( ".quantity " ).text()+'</span>'
+    $(".order_" + id + " .product_order").each(function () {
+        list_products += '<span>' + $(this).children("div").children("span").text() + ' - x' + $(this).children(".quantity ").text() + '</span>'
     });
 
     $(".info_order_product").children('span').remove()
@@ -25,10 +25,10 @@ $('body').on('click', '.show_order_history_code', function() {
 
 
 
-    $(".body_qr_code_"+id).children('canvas').remove()
-    $(".body_qr_code_"+id).children('img').remove()
+    $(".body_qr_code_" + id).children('canvas').remove()
+    $(".body_qr_code_" + id).children('img').remove()
 
-    new QRCode(document.querySelector(".body_qr_code_"+id), {
+    new QRCode(document.querySelector(".body_qr_code_" + id), {
         text: href,
         width: size,
         height: size,
@@ -37,16 +37,16 @@ $('body').on('click', '.show_order_history_code', function() {
         colorLight: "#ffffff"
     });
 
-    $("#code_"+id).modal('show')
+    $("#code_" + id).modal('show')
 })
 
-$('body').on('click', '.close_modal', function() {
+$('body').on('click', '.close_modal', function () {
     id = $(this).attr('data-id')
-    $("#code_"+id).hide()
+    $("#code_" + id).hide()
     $(".modal-backdrop").hide()
 })
 
-$('body').on('click', '.impression_code', function() {
+$('body').on('click', '.impression_code', function () {
     $(".impression_code span").addClass('d-none')
     $(".impression_code div").removeClass('d-none')
 
@@ -62,7 +62,7 @@ function imprimerPages() {
     // IP à mettre dynamiquement
     var printer_ip = $(".printer_id").val() ?? false
 
-    if(!printer_ip){
+    if (!printer_ip) {
         window.print()
     } else {
         ePosDev.connect(printer_ip, "9100", cbConnect, { "eposprint": true });
@@ -71,29 +71,29 @@ function imprimerPages() {
 
 function cbConnect(data, ePos) {
     if (data == 'OK' || data == 'SSL_CONNECT_OK') {
-      var deviceID = "local_printer";
-      ePosDev.createDevice(deviceID, ePosDev.DEVICE_TYPE_PRINTER, { 'crypto': false, 'buffer': false }, cbCreateDevice_printer);
+        var deviceID = "local_printer";
+        ePosDev.createDevice(deviceID, ePosDev.DEVICE_TYPE_PRINTER, { 'crypto': false, 'buffer': false }, cbCreateDevice_printer);
     } else {
-        console.log('Erreur 1:'+data)
+        console.log('Erreur 1:' + data)
         $(".impression_code span").removeClass('d-none')
         $(".impression_code div").addClass('d-none')
         window.print()
-    } 
-  }
+    }
+}
 
 function cbCreateDevice_printer(devobj, retcode) {
-    if( retcode == 'OK' ) {
+    if (retcode == 'OK') {
         printer = devobj;
         printer.timeout = 60000;
-        printer.onreceive = function (res) { 
-            if(!res.success){
+        printer.onreceive = function (res) {
+            if (!res.success) {
                 window.print()
             }
         };
         printer.oncoveropen = function () { alert('coveropen'); };
         printOrder()
     } else {
-        console.log('Erreur 2:'+retcode)
+        console.log('Erreur 2:' + retcode)
         $(".impression_code span").removeClass('d-none')
         $(".impression_code div").addClass('d-none')
         window.print()
@@ -106,8 +106,8 @@ function printOrder() {
     printer.addTextDouble(true, true);
     printer.addTextSize(1, 1);
     printer.addSymbol($(".show #qrcode").attr('title'), printer.SYMBOL_QRCODE_MODEL_2, printer.LEVEL_DEFAULT, 8, 0, 0);
-    $('.show .info_order_product').find('span').each(function(){
-        printer.addText("\n\n"+$(this).text());
+    $('.show .info_order_product').find('span').each(function () {
+        printer.addText("\n\n" + $(this).text());
     });
 
     printer.addText("\n\n\n");
