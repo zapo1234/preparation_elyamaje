8493df9084446c4ac571321a74a741f842d872ab
adrien <adrien@elyamaje.com>
2023-08-21 11:39:45 +0200

    add progress bar for order preparateur

--- assets/js/preparateur.js
+++ assets/js/preparateur.js
@@ -20,20 +20,18 @@ $(document).ready(function () {
                 if (JSON.parse(list_barcode)[k]) {
                     var order_id = JSON.parse(list_barcode)[k].order_id
                     JSON.parse(list_barcode)[k].products.forEach(function (item, key) {
-
-                      
-                        console.log(JSON.parse(list_barcode)[k].quantity)
-                        $("#order_" + order_id + " .barcode_" + item).find('.quantity_pick_in').text(JSON.parse(list_barcode)[k].quantity[key])
-                        if (parseInt($("#order_" + order_id + " .barcode_" + item).find('.quantity_pick_in').text()) ==
-                            parseInt($("#order_" + order_id + " .barcode_" + item).find('.quantity_to_pick_in').text())) {
-                            setTimeout(function () {
-                                $("#order_" + order_id + " .barcode_" + item).addClass('pick')
-                                if($("#"+order_id).text() != "Reprendre"){
-                                    $("#"+order_id).text("Reprendre")
-                                }
-                            }, 0)
+                        if(typeof JSON.parse(list_barcode)[k].quantity != "undefined"){
+                            $("#order_" + order_id + " .barcode_" + item).find('.quantity_pick_in').text(JSON.parse(list_barcode)[k].quantity[key])
+                            if (parseInt($("#order_" + order_id + " .barcode_" + item).find('.quantity_pick_in').text()) ==
+                                parseInt($("#order_" + order_id + " .barcode_" + item).find('.quantity_to_pick_in').text())) {
+                                setTimeout(function () {
+                                    $("#order_" + order_id + " .barcode_" + item).addClass('pick')
+                                    if($("#"+order_id).text() != "Reprendre"){
+                                        $("#"+order_id).text("Reprendre")
+                                    }
+                                }, 0)
+                            }
                         }
-
                     });
 
                     if ($("#order_" + order_id + " .pick").length == $("#order_" + order_id + " .product_order").length) {
