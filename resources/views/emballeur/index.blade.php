cd00e81b6a39f7928229165228f3f268c85be0ad
adrien <adrien@elyamaje.com>
2023-08-24 16:54:32 +0200

    font label

--- resources/views/emballeur/index.blade.php
+++ resources/views/emballeur/index.blade.php
@@ -103,7 +103,7 @@
                     <div class="total_order col"> <b></b> </div>
                     <div class="text-center total_product_order col"> <b></b> </div>
 
-                    <div class="amount_total_order col d-flex justify-content-end"> <b></b> </div>
+                    <div class="text-end amount_total_order col d-flex justify-content-end"> <b></b> </div>
                 </div>
                 <button disabled type="button" class="empty_order validate_order btn btn-primary d-flex mx-auto"> Valider </button>
             </div>
