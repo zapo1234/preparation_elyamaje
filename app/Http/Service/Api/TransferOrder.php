3e74477009e9585baffd013a8eb8755d8215bff8
martial <martial@elyamaje.com>
2023-08-28 15:09:58 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -605,8 +605,6 @@ class TransferOrder
                   // recupérer le mode de paiement
                   $account_name = $this->getAccountpay();
 
-                  dd($account_name);
-
                   if($account_name==""){
                     $account_name="vir_card";
                   }
