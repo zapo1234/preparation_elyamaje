76e610adb1a03973662519288174e41d41480e8c
adrien <adrien@elyamaje.com>
2023-08-28 17:31:41 +0200

    test

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -404,9 +404,11 @@ class Order extends BaseController
 
     public function validWrapOrder(Request $request){
           
+
       // $order_id = $request->post('order_id');
       $order_id = 80283; // Données de test
       $order = $this->order->getOrderByIdWithCustomer($order_id);
+      dd($order);
 
       if($order){
 
