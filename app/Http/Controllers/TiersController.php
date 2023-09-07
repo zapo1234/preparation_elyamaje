cf36cdd83112a4865ce0d3d8a8c4bfc5b1573613
martial <martial@elyamaje.com>
2023-09-07 12:08:08 +0200

    p

--- app/Http/Controllers/TiersController.php
+++ app/Http/Controllers/TiersController.php
@@ -175,10 +175,12 @@ class TiersController extends BaseController
          foreach($data as $values){
           $date = date('Y-m-d', $values['datem']);
            if($values['array_options']!=""){
+             foreach($values['array_options'] as $valu){
                    if($date==$datet){
-                     $ids_commande[] = $values['array_options']['options_idw'];
+                     $ids_commande[] = $valu['options_idw'];
                   }
                }
+             }
            }
             dd($ids_commande);
        }
