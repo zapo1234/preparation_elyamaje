648c4129f625e811fe22207579fe43f780bcc31a
martial <martial@elyamaje.com>
2023-09-07 13:55:24 +0200

    p

--- app/Repository/Tiers/TiersRepository.php
+++ app/Repository/Tiers/TiersRepository.php
@@ -179,7 +179,7 @@ class TiersRepository implements TiersInterface
             foreach($listproduct as $valu){
                  foreach($valu['array_options'] as $val)
                      if($val!=""){
-                      $lists[] =$val['options_idw'];
+                      $lists[] =$val;
                  }
               }
 
