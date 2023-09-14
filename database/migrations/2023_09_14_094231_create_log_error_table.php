a7beef2cf7afb7357605f5ea75131128269188b6
adrien <adrien@elyamaje.com>
2023-09-14 13:51:23 +0200

    add new table

---
+++ database/migrations/2023_09_14_094231_create_log_error_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogErrorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_error', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->nullable();
+            $table->string('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_error');
+    }
+}
