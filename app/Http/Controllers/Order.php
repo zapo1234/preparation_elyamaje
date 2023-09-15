b560bc49130d9f90ab75b5110e823a102c1826b6
martial <martial@elyamaje.com>
2023-09-15 15:53:27 +0200

    p

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -421,7 +421,7 @@ class Order extends BaseController
     public function validWrapOrder(Request $request){
           
       //$order_id = $request->post('order_id');
-      $order_id =93803; // Données de test
+      $order_id =94399; // Données de test
       $order = $this->order->getOrderByIdWithCustomer($order_id);
 
     
