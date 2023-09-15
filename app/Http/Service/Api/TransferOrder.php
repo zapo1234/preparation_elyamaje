e80d32ac3dc24fed953081afab9223bd3b372870
martial <martial@elyamaje.com>
2023-09-15 17:13:36 +0200

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -492,7 +492,7 @@ class TransferOrder
                         */
 
                     
-                        dd($data_lines);
+                        
                         
                          // Create le client via Api...
                         foreach($data_tiers as $data) {
