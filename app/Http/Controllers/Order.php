3812f3e244a18bc00013d32673bdf4a24bb9403e
martial <martial@elyamaje.com>
2023-09-15 16:25:10 +0200

    p

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -421,7 +421,7 @@ class Order extends BaseController
     public function validWrapOrder(Request $request){
           
       //$order_id = $request->post('order_id');
-      $order_id =94489; // Données de test
+      $order_id =94509; // Données de test
       $order = $this->order->getOrderByIdWithCustomer($order_id);
 
     
--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -490,6 +490,10 @@ class TransferOrder
                          }
                         */
                          
+                        dump($data_tiers);
+
+                        dd($data_lines);
+                         
                          // Create le client via Api...
                         foreach($data_tiers as $data) {
                            // insérer les données tiers dans dolibar
