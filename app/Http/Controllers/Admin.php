b26cc590c169f46a678bcce031178f1d192621b6
adrien <adrien@elyamaje.com>
2023-09-21 13:03:17 +0200

    get hostname

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -278,22 +278,6 @@ class Admin extends BaseController
 
     public function analytics(){
 
-
-        // if user from the share internet  
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
-            echo 'IP address = '.$_SERVER['HTTP_CLIENT_IP'];  
-        }  
-        //if user is from the proxy  
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-            echo 'IP address = '.$_SERVER['HTTP_X_FORWARDED_FOR'];  
-        }  
-        //if user is from the remote address  
-        else{  
-            echo 'IP address = '.$_SERVER['REMOTE_ADDR'];  
-        }    
-
-        exit;
-
         $date = date('Y-m-d');
         $histories = $this->history->getHistoryAdmin($date);
         $list_histories = [];
