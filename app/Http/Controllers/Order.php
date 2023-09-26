dfadebc28492b7bbd564e2713b781c0e0b5ebcd7
adrien <adrien@elyamaje.com>
2023-09-26 11:09:34 +0200

    order already billing

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -469,6 +469,7 @@ class Order extends BaseController
 
         } catch(Exception $e){
           $this->logError->insert(['order_id' => $order_id, 'message' => $e->getMessage()]);
+          echo json_encode(['success' => true, 'message' => 'Commande '.$order[0]['order_woocommerce_id'].' préparée avec succès !']);
         }
 
         // Génère l'étiquette ou non
