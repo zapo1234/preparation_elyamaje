14328e75695b6b2220dce384f624a3102a87349d
martial <martial@elyamaje.com>
2023-10-31 10:23:39 +0100

    p

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -642,14 +642,11 @@ class Order extends BaseController
 
     public function validWrapOrder(Request $request){
       
-     // $from_dolibarr = $request->post('from_dolibarr') == "false" ? 0 : 1;
-      //$transfers = $request->post('transfers') == "false" ? 0 : 1;
+      $from_dolibarr = $request->post('from_dolibarr') == "false" ? 0 : 1;
+      $transfers = $request->post('transfers') == "false" ? 0 : 1;
       // Sécurité dans le cas ou tout le code barre est envoyé, on récupère que le numéro
-      //$order_id = explode(',', $request->post('order_id'))[0];
+      $order_id = explode(',', $request->post('order_id'))[0];
 
-      $from_dolibarr =false;
-      $transfers =false;
-      $order_id =100341;
       if($from_dolibarr){
         // Si commande dolibarr je fournis le fk_command
         $order = $this->orderDolibarr->getOrdersDolibarrById($order_id);
