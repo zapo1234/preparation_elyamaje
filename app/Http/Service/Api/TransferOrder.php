302429ec48873be5217832cfd8bd2f5b3bccd8c8
martial <martial@elyamaje.com>
2023-12-01 10:37:39 +0100

    p

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -1135,7 +1135,10 @@ class TransferOrder
                 foreach($data_fk_facture as $lk){
                    $deletepaiement  = DB::connection('mysql2')->select("DELETE FROM llxyq_paiement_facture WHERE fk_facture=$lk");
                    // suprimer ecriture paiement
-                   $deletepaiement  = DB::connection('mysql2')->select("DELETE FROM llxyq_paiement WHERE fk_facture=$lk");
+                }
+                foreach($ref_facture as $ref){
+                   // suprimer les ligne d'ecriture de paiement avec la ref facture.
+                   $deletepaiement  = DB::connection('mysql2')->select("DELETE FROM llxyq_paiement WHERE num_paiement=$ref");
                 }
 
                 dd('zapo');
