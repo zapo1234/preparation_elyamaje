64051e40db53a1653a9b30b41580fa99d7c55809
elyamaje <adrien@elyamaje.com>
2024-02-28 16:56:55 +0100

    css

--- resources/views/admin/logs.blade.php
+++ resources/views/admin/logs.blade.php
@@ -51,9 +51,9 @@
 								<tbody>
 									@foreach ($logs as $log)
 										<tr>
-											<td class="col-md-1 text-dark font-bold">{{ $log['order_id'] }}</td>
+											<td class="col-md-1 font-bold" style="color: black !important">{{ $log['order_id'] }}</td>
 											<td><span style="word-break:break-word" class="text-danger">{{ $log['message'] }}</span></td>
-											<td class="col-md-2 text-dark font-bold">{{ $log['created_at'] }}</td>
+											<td class="col-md-2 font-bold" style="color: black !important">{{ $log['created_at'] }}</td>
 											<td class="col-md-2">{{ $log['id'] }}</td>
 										</tr>
 									@endforeach
