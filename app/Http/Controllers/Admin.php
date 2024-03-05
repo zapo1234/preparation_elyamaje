3eaf748b9fb98dfd020de61512677df2acb9ae25
martial <martial@elyamaje.com>
2024-03-05 12:34:27 +0100

    admin facture

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -667,8 +667,8 @@ class Admin extends BaseController
 
             try {
 
-                    //$this->transfers->Transfertext($order);
-                    $this->factorder->Transferorder($order);  
+                    $this->transfers->Transfertext($order);
+                    //$this->factorder->Transferorder($order);  
 
                 // Stock historique
                 $data = [
