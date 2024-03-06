e89bcf5b91bb4c39c3aabd3194959c21db52031c
martial <martial@elyamaje.com>
2024-03-06 10:40:23 +0100

    envoi facture

--- app/Http/Service/PDF/InvoicesPdf.php
+++ app/Http/Service/PDF/InvoicesPdf.php
@@ -19,7 +19,7 @@ class InvoicesPdf
           $this->pdf = $pdf;
       }
 
-       public function invoicespdf($data_line_order,$tiers,$ref_order,$total_ht,$total_ttc,$destinataire,$code_promo,$remise,$indexs){
+       public function invoicespdf($data_line_order,$tiers,$ref_order,$total_ht,$total_ttc,$destinataire,$code_promo,$remise,$percent,$indexs){
         
          $date = date('Y-m-d H:i:s');
          $date = date('d/m/Y');
@@ -27,8 +27,7 @@ class InvoicesPdf
          // $destinataire ='zapomartial@yahoo.fr';
          $subject = 'Confirmation de commande Elyamaje lors de la Beauty Prof Paris 2024';
          $content = 'Bonjour Merci de recevoir votre fature de commande';
-
-         dd($indexs);
+         
         
          try{
 
@@ -47,7 +46,7 @@ class InvoicesPdf
             // envoi de mail au client.
         
             if($indexs=="xxxv1"){
-            Mail::send('email.invoice', ['ref_order'=>$ref_order,'code_promo'=>$code_promo], function ($message) use ($to, $subject, $content,$path_invoice) {
+            Mail::send('email.invoice', ['ref_order'=>$ref_order,'code_promo'=>$code_promo,'percent'=>$percent], function ($message) use ($to, $subject, $content,$path_invoice) {
                       $message->to($to);
                       $message->subject($subject);
                       $message->from('no-reply@elyamaje.com');
