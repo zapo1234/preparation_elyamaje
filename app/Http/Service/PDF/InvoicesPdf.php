ac3debc9aa32fe91477568c43716074d68c3e30e
martial <martial@elyamaje.com>
2024-03-06 10:16:42 +0100

    p

--- app/Http/Service/PDF/InvoicesPdf.php
+++ app/Http/Service/PDF/InvoicesPdf.php
@@ -19,11 +19,9 @@ class InvoicesPdf
           $this->pdf = $pdf;
       }
 
-       public function invoicespdf($data_line_order,$tiers,$ref_order,$total_ht,$total_ttc,$destinataire,$code_promo,$remise,$indexs){
-         //$ref_order="BP-marseille-000001";
-         $date = date('Y-m-d H:i:s');
-         $date = date('d/m/Y');
-          dd($indexs);
+       public function invoicespdf($data_line_order,$tiers,$ref_order,$total_ht,$total_ttc,$destinataire,$code_promo,$remise,$percent,$indexs){
+        
+         dd($indexs);
         // $destinataire ='zapomartial@yahoo.fr';
         $subject = 'Confirmation de commande Elyamaje lors de la Beauty Prof Paris 2024';
         $content = 'Bonjour Merci de recevoir votre fature de commande';
