8ef176591e2e25435105885fdfd1e9ac039b93de
martial <martial@elyamaje.com>
2024-03-14 10:19:47 +0100

    mise a jours point fidelite

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -600,7 +600,7 @@ class TransferOrder
                                     }else{
                                           $index_int="";
                                           $total_shipping = $donnees['shipping_amount']*1.2;
-                                          $montant_fidelite = $donnees['total_order']-$total_shipping;
+                                          $montant_fidelite = $donnees['total_order']-$total_shipping+$donnees['gift_card_amount'];
                                           
                                     }
 
