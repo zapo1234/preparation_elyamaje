11178f9b68b1437976269f451e33a86747879835
martial <martial@elyamaje.com>
2024-03-18 14:18:37 +0100

    function test

--- app/Http/Service/Api/TransferOrder.php
+++ app/Http/Service/Api/TransferOrder.php
@@ -717,7 +717,7 @@ class TransferOrder
 
                        foreach($data_lines as  $val){
 
-                          for($i=0; $i<335;$i++){
+                          for($i=0; $i<300;$i++){
                             $lines[]= $val['lines'][$i];
                             
                            }
