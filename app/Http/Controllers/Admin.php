ff8ecf85f6790c75dbe64dbfaf35418fa4b26258
martial <martial@elyamaje.com>
2024-03-18 10:23:16 +0100

    p

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -680,8 +680,8 @@ class Admin extends BaseController
 
             try {
 
-                     $this->transfers->Transfertext($order);
-                    // $this->factorder->Transferorder($order);  
+                    // $this->transfers->Transfertext($order);
+                     $this->factorder->Transferorder($order);  
 
                 // Stock historique
                 $data = [
