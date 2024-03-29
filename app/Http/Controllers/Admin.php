a233e589eb2ae28ee5f4ae2465a23a2d1ee9d0ff
Lyes BELKHOUS <lyes@elyamaje.com>
2024-03-29 17:03:30 +0100

    correction du dd dans le code

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -841,8 +841,6 @@ class Admin extends BaseController
         try {
             $pdoDolibarr = new PdoDolibarr(env('HOST_ELYAMAJE'),env('DBNAME_DOLIBARR'),env('USER_DOLIBARR'),env('PW_DOLIBARR'));
 
-            dd($pdoDolibarr);
-
             $tab_categories = $pdoDolibarr->getCategoriesDolibarr();
            
             DB::beginTransaction();
