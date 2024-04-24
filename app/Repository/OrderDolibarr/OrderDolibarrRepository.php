eefdbfe802f81f79efc6d4f8ba194decbf2f7f1c
elyamaje <adrien@elyamaje.com>
2024-04-24 15:01:54 +0200

    correction error

--- app/Repository/OrderDolibarr/OrderDolibarrRepository.php
+++ app/Repository/OrderDolibarr/OrderDolibarrRepository.php
@@ -396,8 +396,9 @@ class OrderDolibarrRepository implements OrderDolibarrInterface
    }
 
    public function getAllOrdersAndLabel(){
+      
       $date = date('Y-m-d');
-      $results = $this->model::select('orders_doli.id as order_woocommerce_id', 'orders_doli.fk_commande', 'orders_doli.statut as status', /*'label_product_order.*',*/ 'labels.tracking_number', 'labels.created_at as label_created_at', 'labels.label_format', 
+      $results = $this->model::select('orders_doli.id as order_woocommerce_id', 'orders_doli.fk_commande', /*'label_product_order.*',*/ 'labels.tracking_number', 'labels.created_at as label_created_at', 'labels.label_format', 
       'labels.cn23', 'labels.download_cn23', 'labels.origin', 'labels.id as label_id')
       // ->Leftjoin('label_product_order', 'label_product_order.order_id', '=', 'orders_doli.id')
       ->Leftjoin('labels', 'labels.order_id', '=', 'orders_doli.ref_order')
