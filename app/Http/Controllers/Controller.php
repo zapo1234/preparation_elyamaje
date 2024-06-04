0882a83cafc2f8d53d2cc5b771d183d452007e8d
martial <martial@elyamaje.com>
2024-06-04 09:13:02 +0200

    envoi de la fonction facturation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2776,8 +2776,6 @@ class Controller extends BaseController
                         $order['gala'] = false;
                     }
 
-                    dd($order);
-
                     $order_to_billing[] = $order;
                     if(count($order_to_billing) == 4){
                         // Envoie à la facturation par 4
