7ae7c3fe5f3be336d39bbfedbe6a914b2e38ee3b
martial <martial@elyamaje.com>
2024-06-28 12:27:25 +0200

    envoi orders in function

--- app/Http/Service/Api/Transfertext.php
+++ app/Http/Service/Api/Transfertext.php
@@ -619,7 +619,7 @@ class Transfertext
                                      // gérer les moyens de transport de collisimo
                                        $array_line_product =[];
                                        $total_a_tva = $donnees['shipping_amount']*20/100;
-                                       $total_product=20;
+                                       $tva_product=20;
                                        if($donnees['shipping_method_detail']!=""){
                                           $array_line_product[]=[
                                            "desc"=>$donnees['shipping_method_detail'],
@@ -631,7 +631,7 @@ class Transfertext
                                            "product_type"=>'1',
                                            "product_label" =>'',
                                             "qty" => '1',
-                                            "fk_product" =>'',//  insert id product dans dolibar.
+                                            "fk_product" =>'',//  insert id product dans dolibar...
                                             "tva_tx" => floatval($tva_product),
                                             "ref_ext" => $socid, // simuler un champ pour socid pour identifié les produit du tiers dans la boucle /****** tres bon
                                          ];
