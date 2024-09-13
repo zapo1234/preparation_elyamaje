0e35ff5214f55c5bb040bad09ce57b103e9d3bae
zapo1234 <zapomartial@yahoo.fr>
2024-09-13 22:12:41 +0200

    correction variable

--- app/Http/Controllers/TiersController.php
+++ app/Http/Controllers/TiersController.php
@@ -272,7 +272,7 @@ class TiersController extends BaseController
    
          // resultat la table historique 
          $resultat_histories = $list_array;
-         $data = array_unique($list);
+         //$data = array_unique($list);
    
          // chercher les diff entre les deux tableau. 
          $diff_array = array_diff($resultat_histories,$array_finale);
