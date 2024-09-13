c26bfb2a0b90cbd3c0f5c62aeca697f9e6a51477
zapo1234 <zapomartial@yahoo.fr>
2024-09-13 22:03:48 +0200

    tester function controle

--- app/Http/Controllers/TiersController.php
+++ app/Http/Controllers/TiersController.php
@@ -255,7 +255,7 @@ class TiersController extends BaseController
       }
 
       // recupérer les id order qui sont en payé
-      dd(array_filter($array_final));
+      $array_finale = array_filter($array_final);
 
         
          // recupérer les ids de produits dans ce intervale.
