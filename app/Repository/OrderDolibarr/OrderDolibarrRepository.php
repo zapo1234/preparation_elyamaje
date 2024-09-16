3bcd20d49c0011d46c77d6b98910a4502a605add
zapo1234 <zapomartial@yahoo.fr>
2024-09-16 22:08:44 +0200

    'ps

--- app/Repository/OrderDolibarr/OrderDolibarrRepository.php
+++ app/Repository/OrderDolibarr/OrderDolibarrRepository.php
@@ -986,11 +986,12 @@ class OrderDolibarrRepository implements OrderDolibarrInterface
 
               // aller cherher dans la table tickera les code
               $data_ticket_code = DB::table('tickera')
-              ->select('code_reduction') // Spécifiez les colonnes à sélectionner
+              ->select('ticket_id','code_reduction') // Spécifiez les colonnes à sélectionner
                ->whereIn('ticket_id', $ref_ticket)
                ->get();
                 $data_tickeras = json_decode($data_ticket_code,true);
                 // recupérer dans un tableau unique les data code
+                dd($data_tickeras);
                foreach($data_tickeras as $vals){
 
                    $data_code[] = $vals['code_reduction'];
