a28fef3ecc4f289b127fcb62e7b4f02a5e388ec4
zapo1234 <zapomartial@yahoo.fr>
2024-09-19 22:56:37 +0200

    data

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -2134,7 +2134,7 @@ class Admin extends BaseController
     foreach($datas as $key => $values){
      // verifier si le fk_product est dans une chain
      foreach($values as $value){
-       if(strpos($value['products'],$fk_product_billet)!=false){
+       if(strpos($value['products'],$fk_product_billet)!==false){
           $data_fk_invoice[] = $value['fk_product'];
 
        }
