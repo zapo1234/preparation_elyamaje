693393d0c5aa8983b2d68462386fddae26d3ecf7
zapo1234 <zapomartial@yahoo.fr>
2024-09-19 23:43:05 +0200

    recupera data

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -2133,10 +2133,9 @@ class Admin extends BaseController
    $data_fk_invoice =[];
     foreach($datas as  $values){
      // verifier si le fk_product est dans une chain
-        if(strpos($values['products'],$fk_product_billet)!==false){
+    dump($values['fk_product']);
 
-           $data_fk_invoice[] = $values['fk_product'];
-        }
+    dd($values['products']);
      
     }
 
