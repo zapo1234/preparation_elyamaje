09fc5ad7264a7e9c4c80a623551d461cfa532f83
zapo1234 <zapomartial@yahoo.fr>
2024-09-26 22:20:29 +0200

    data invoices

--- app/Http/Service/PDF/InvoicesPdf.php
+++ app/Http/Service/PDF/InvoicesPdf.php
@@ -44,7 +44,7 @@ class InvoicesPdf
             $path_invoice = "storage/app/$filePaths";
 
              //$to="adrien@elyamaje.com";
-              $to ="emilie@elyamaje.com";
+              $to ="lyes@elyamaje.com";
              //$to = $tiers['email'];
             // envoi de mail au client.
             
