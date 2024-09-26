ea674a6fcc0e00966668c1cec10736d81d42b9ad
zapo1234 <zapomartial@yahoo.fr>
2024-09-26 19:41:13 +0200

    datasé

--- app/Http/Service/PDF/InvoicesPdf.php
+++ app/Http/Service/PDF/InvoicesPdf.php
@@ -44,7 +44,7 @@ class InvoicesPdf
             $path_invoice = "storage/app/$filePaths";
 
              //$to="adrien@elyamaje.com";
-              $to ="adrien@elyamaje.com";
+              $to ="zapomartial@yahoo.fr";
              //$to = $tiers['email'];
             // envoi de mail au client.
             
